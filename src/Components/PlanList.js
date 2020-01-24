0ed4ab9835e3e7a28fa6d3ed53ab497c7554d7ec
Mshary <themshary@Msharys-MacBook-Pro.local>
2020-01-24 17:07:00 +0300

    Plan list page complete

--- src/Components/PlanList.js
+++ src/Components/PlanList.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 import { observer } from "mobx-react";
+import { Link } from "react-router-dom";
+
+// Grommet
+import { Main, Heading } from "grommet";
 
 import planStore from "../stores/planStore";
 
 class PlanList extends Component {
   render() {
     const plans = planStore.plans.map(plan => (
-      <>
-        {plan.datetime} at {plan.location}
-      </>
+      <Link to={`/plans/${plan.id}`} key={plan.id}>
+        <Heading level={3}>
+          {plan.datetime} at {plan.location}
+        </Heading>
+      </Link>
     ));
 
-    return <div>{plans}</div>;
+    return (
+      <Main pad="large" align="center">
+        {plans}
+      </Main>
+    );
   }
 }
 
